3cd10c61ce58b5972cdc91287cd6dbb6cf834d74
NSILOU BALANDAMIO perseverance <nsiloup@gmail.com>
2022-01-22 13:52:45 +0100

    serving a file with response.sendFile(PATH) METHOD

--- myApp.js
+++ myApp.js
@@ -1,45 +1,11 @@
 var express = require('express');
 var app = express();
 
+let path = __dirname + "/views/index.html"
+
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.sendFile(path)
   })
 
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
