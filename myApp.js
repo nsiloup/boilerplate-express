95acd3ba15da2731e14c523d61b25a6b9c56522c
NSILOU BALANDAMIO perseverance <nsiloup@gmail.com>
2022-01-23 00:06:26 +0100

    serving Json

--- myApp.js
+++ myApp.js
@@ -3,9 +3,12 @@ var app = express();
 
 let path =(rel)=> __dirname + rel;
 
+app.use("/public", express.static(path("/public")));
 app.get('/', (req, res) => {
     res.sendFile(path("/views/index.html"))
 })
-app.use("/public", express.static(path("/public")));
+
+
+app.get('/json', (req, res)=>res.json({"message": "Hello json"}))
 
 module.exports = app;
