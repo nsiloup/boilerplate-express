ba4be665fb4c2cd38bd1386e6802907a30dd4e10
NSILOU BALANDAMIO perseverance <nsiloup@gmail.com>
2022-01-25 07:41:24 +0100

    !

--- myApp.js
+++ myApp.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 var express = require('express');
 var app = express();
 
+let logger = (req, res, next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+}
+app.use(logger)
+
+
+
 let path =(rel)=> __dirname + rel;
 
 app.use("/public", express.static(path("/public")));
